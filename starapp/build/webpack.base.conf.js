449ec03cc9b8f1c664dff3897c6316d011aaad0f
chenbowen950908 <416625434@qq.com>
2017-09-08 12:30:14 +0800

    修改打包方式

--- starapp/build/webpack.base.conf.js
+++ starapp/build/webpack.base.conf.js
@@ -21,9 +21,9 @@ let webpackConfig = {
 	output: {
 		path: config.build.assetsRoot,
 		filename: '[name].js',
-		publicPath: process.env.NODE_ENV === 'production' ?
-			config.build.assetsPublicPath : config.dev.assetsPublicPath
-		//		publicPath: './'
+//		publicPath: process.env.NODE_ENV === 'production' ?
+//			config.build.assetsPublicPath : config.dev.assetsPublicPath
+		publicPath: './'
 	},
 	resolve: {
 		extensions: ['.js', '.vue', '.json'],
